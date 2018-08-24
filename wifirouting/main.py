c3c7637eaa190d61b76b2806d98511916c289a0e
Andy Radford <andy@andyradford.co.uk>
2018-08-24 09:50:17 +0100

    starting to tidy up

--- wifirouting/main.py
+++ wifirouting/main.py
@@ -1,19 +1,16 @@
-import config
 import os
-import requests
-import xml.etree.cElementTree as ET
-import re
+
 import math
 import pickle
 import time
 import folium
-import os
 from collections import defaultdict
 
-import config
 import geometry
 import shortest
 import sampleget
+import journeytimeinfo
+import dik
 
 def dist(x):
 	a1 = x[0][0]
@@ -34,25 +31,6 @@ def minmax(a):
 	maxy+=(maxx-minx)*.2
 	return (minx, miny), (maxx, maxy)
 
-
-class cache:
-	def __init__(self):
-		try:
-			with open('cache.p', 'r') as pfile:
-				self.c = pickle.load(pfile)
-		except:
-			self.c = {}
-
-	def datagrab(self, url):
-		par = {'ApiKey': os.environ['ALKEY']}
-		try:
-			n = requests.get(url, params=par, timeout=1)
-		except:
-			return self.c[url].content
-		self.c[url] = n
-		with open('cache.p', 'w') as pfile:
-			pickle.dump(self.c, pfile)
-		return n.content
 
 class offset():
 	def __init__(self):
@@ -90,7 +68,7 @@ def setupshortest(plt, ln, plt1, ln1, t, b, a, origang):
 	else:
 		ns += [[ln1[0], -2, shortest.distance(n1, plt1)/4]]
 	try:
-		temp = list(shortest.Flatten(shortest.Dijkstra(a + ns, -1,-2)))
+		temp = list(shortest.Flatten(dik.Dijkstra(a + ns, -1,-2)))
 		dist = temp[0]
 		temp = temp[2:-1]
 	except:
@@ -99,60 +77,15 @@ def setupshortest(plt, ln, plt1, ln1, t, b, a, origang):
 	return dist, temp
 
 def main():
-	#print n.content
-	f = cache()
-	root = ET.fromstring(f.datagrab('http://bcc.opendata.onl/UTMC Flow.xml'))
-	locs = {}
-	for flow in root.findall('Flow'):
-		try:
-			a = flow.find('SCN').text
-			b = float(flow.find('Northing').text)
-			c = float(flow.find('Easting').text)
-			for val in flow.findall('Value'):
-				d = int(val.find('Level').text)
-		except AttributeError:
-			continue
-		b, c = geometry.OSGB36toWGS84(c, b)
-		#b,c=geometry.deg2num(c,b,zoom=13)
-		#print a,((b,c),d)
-		locs[a] = ((b, c), d)
-	
-	#print n.content
-	root = ET.fromstring(f.datagrab('http://bcc.opendata.onl/UTMC ANPR.xml'))
-	ret = {}
-	for flow in root.findall('ANPR'):
-		try:
-			a = flow.find('Description').text
-			b = flow.find('SCN').text
-			c = flow.find('Plates').attrib['matched']
-			d = flow.find('Time').text
-		except AttributeError:
-			continue
-		#print a,b,c,d
-		tmp = []
-		for t in a.split(' '):
-			n = re.findall(r'E\d\d\d\d', t)
-			if n:
-				tmp.append(n[0][1:])
-		if tmp:
-			try:
-				ret[b] = {
-					'coordinates': [locs[tmp[0]][0], locs[tmp[1]][0]],
-					'Time': d,
-					'Description': a,
-					'matched': c
-				}
-			except KeyError:
-				print tmp[0], tmp[1]
-	
+	#get data on the links from the portal
+	ret=journeytimeinfo.getwifidata()
+	# work out how much large the network needs to be
+	x, y = minmax([ret[x]['coordinates'][0] for x in ret] + [ret[x]['coordinates'][1] for x in ret])
+	# update the sampledata if it is out of data
 	old = time.time() - os.path.getmtime('sampledata.pickle')
-	x, y = minmax([ret[x]['coordinates'][0]
-																for x in ret] + [ret[x]['coordinates'][1] for x in ret])
-	
 	if old > 400060: 
 		sampleget.osmcreate(x, y)
 	
-	map_osm = folium.Map(location=(52.483678100000005, -1.8201131000000134), zoom_start=14)
 	ctr = 0
 	res1=[]
 	offdata=offset()
@@ -160,44 +93,21 @@ def main():
 		osm = pickle.load(file)
 		a, b = shortest.OsmRouteImport(osm)  #a-links,b-nodes
 		minx, maxx, miny, maxy, ratio = shortest.findminmax(b)
-		col = ['red', 'green', 'blue']
-		col=['#fff7f3','#fde0dd','#fcc5c0','#fa9fb5','#f768a1','#dd3497','#ae017e','#7a0177','#49006a']
+
 	for n in ret:
 		ctr+=1
 		print int((float(ctr) / len(ret))*100),"%"
-		#print ret[n]['coordinates'][0][0]
 		t = geometry.deg2num(ret[n]['coordinates'][0][0], ret[n]['coordinates'][0][1], zoom=13)
 		t1 = geometry.deg2num(ret[n]['coordinates'][1][0], ret[n]['coordinates'][1][1], zoom=13)
-		origang = (shortest.angle(t, t1))
+		origang = (shortest.angle(t, t1)) #not currently required
 		r2 = []
+		tst=time.time()
 		zt=shortest.getlocsa(t1, b, a, origang)
 		for start in shortest.getlocsa(t, b, a, origang):
 			for end in zt:
 				r2.append(
 					setupshortest(start[0], start[1], end[0], end[1], t, b, a, origang))
-				'''if r2[-1][0] > 0 and r2[-1][0]<100000:
-					print r2[-1][0],len(r2[-1][1])'''
-		'''
-		for nn in r2:
-			print nn[0]
-			try:
-				folium.Marker(
-				geometry.num2deg(nn[1][0][0], nn[1][0][1], 13),
-				icon=folium.Icon(color='green'),popup='start').add_to(map_osm)
-	
-				folium.Marker(
-				geometry.num2deg(nn[1][-1][0], nn[1][-1][1], 13),
-				icon=folium.Icon(color='green'), popup='end').add_to(map_osm)
-			except:
-				continue
-		'''
-	
-		#print len(r2)
-		
-			#temp=[x for x in r2 if x[0]>0]
-			#for nn in r2:
-			#print nn[0],len(nn[1])
-
+		print time.time()-tst
 		tmp=[(x[0],x[1]) for x in min(r2, key=lambda x: x[0])[1]]
 		res1.append(tmp)
 		if ctr == 2:
@@ -206,7 +116,12 @@ def main():
 	import json
 	with open ('links.json','w') as jsonfile:
 		json.dump(res1,jsonfile)
-		
+
+	#this section draws the map
+
+	map_osm = folium.Map(location=(52.483678100000005, -1.8201131000000134), zoom_start=14)
+	col = ['red', 'green', 'blue']
+	col=['#fff7f3','#fde0dd','#fcc5c0','#fa9fb5','#f768a1','#dd3497','#ae017e','#7a0177','#49006a']
 	ctr=0
 	vals=[]
 	for n in res1:
@@ -230,18 +145,8 @@ def main():
 			temp, color='black',weight=8).add_to(map_osm)
 		folium.PolyLine(
 			temp, popup='try later',weight=7, color=col[ctr % 9]).add_to(map_osm)
-		''folium.Marker(ret[n]['coordinates'][0]).add_to(map_osm)
-		folium.Marker(ret[n]['coordinates'][1]).add_to(map_osm)'''
 		ctr+=1
 
-	
-	'''
-	to draw
-	sort lines by length
-	draw longest first
-	if line exists in database - increase its offset by 10
-	'''
-	
 	
 	map_osm.save('map.html')
 	import sys
