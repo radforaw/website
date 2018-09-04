e35095bdba941efa29e295f7d1d1807a8281a17a
Ubuntu <ubuntu@ip-172-31-87-2.ec2.internal>
2018-09-04 21:58:02 +0000

    added bristol file

---
+++ yourapp/pycode/wifirouting/bristol.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  bristol.py
+#  
+#  Copyright 2018  <pi@raspberrypi>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+import requests
+try:
+	from lxml import etree as ET
+except ImportError:
+	import xml.etree.cElementTree as ET
+key={"apikey":"QAPLPKaFokiEkmpv7oFDSw"}
+surl="https://highways-bristol.api.urbanthings.io/api/1.0/dynamic/vms/status"
+durl="https://highways-bristol.api.urbanthings.io/api/1.0/dynamic/vms/definition"
+ns="{http://datex2.eu/schema/2/2_0}"
+
+def main():
+	defs={}
+	n=requests.get(durl,params=key)
+	root=ET.fromstring(n.content)
+	for a in root.iter(ns+"vmsUnitRecord"):
+		t=a.attrib['id']
+		for b in a.iter(ns+"latitude"):
+			tlat=float(b.text)
+		for b in a.iter(ns+"longitude"):
+			tlon=float(b.text)
+		defs[t]=[[tlat,tlon]]
+	n=requests.get(surl,params=key)
+	root=ET.fromstring(n.content)
+	for a in root.iter(ns+"vmsUnit"):
+		p=a.find(ns+'vmsUnitReference')
+		tmp=""
+		for b in a.iter(ns+'vmsText'):
+			for c in b.findall(ns+'vmsTextLine'):
+				for d in c.findall(ns+'vmsTextLine'):
+					for e in d.findall(ns+'vmsTextLine'):
+						if e.text:
+							tmp+=e.text+"|"
+		defs[p.attrib['id']].append(tmp)
+	for n in defs:
+		if len(defs[n])<>2:
+			defs[n].append("")
+	print defs
+	return [['Null',defs[h][1],defs[h][0],h] for h in defs]
+
+if __name__ == '__main__':
+	print main()
